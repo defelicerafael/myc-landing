5c238c8ad5e8cdc136a1350a2b85650d266aff4c
Rafu Defelice <defelicerafael@gmail.com>
2023-11-14 11:08:27 -0300

    Hasta ahora 14/11/2023

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BigTextComponent } from './home/big-text/big-text.component';
+import { ImagesComponent } from './home/images/images.component';
+import { ImgTextComponent } from './home/img-text/img-text.component';
+import { IconsComponent } from './home/icons/icons.component';
+import { ContactComponent } from './home/contact/contact.component';
+import { CarouselComponent } from './home/carousel/carousel.component';
+import { FooterComponent } from './home/footer/footer.component';
 
 @NgModule({
   declarations: [
@@ -10,7 +17,15 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BigTextComponent,
+    ImagesComponent,
+    ImgTextComponent,
+    IconsComponent,
+    CarouselComponent,
+    ContactComponent,
+    FooterComponent
+
   ],
   providers: [],
   bootstrap: [AppComponent]
